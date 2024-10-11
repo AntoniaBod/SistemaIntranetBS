bea76710d93c0ca6653a2dfddf596f3fe8b0f99a
AntoniaBod <143849382+AntoniaBod@users.noreply.github.com>
2024-10-10 21:11:39 -0300

    Atualização

--- TEMPLATES/listarFunc_admin.php
+++ TEMPLATES/listarFunc_admin.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Funcionários</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../CSS/style1.css"> <!-- Adicione o caminho correto para o seu CSS -->
 </head>
 <body>
