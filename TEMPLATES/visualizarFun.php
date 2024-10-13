49dc3dc5bf85bb819c359f15ea8c331b66babcc0
AntoniaBod <143849382+AntoniaBod@users.noreply.github.com>
2024-10-13 20:55:47 -0300

    Atualização

--- TEMPLATES/visualizarFun.php
+++ TEMPLATES/visualizarFun.php
@@ -33,7 +33,7 @@ if (isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visualizar Funcionário</title>
-    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="../CSS/style1.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
