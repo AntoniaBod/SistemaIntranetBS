c6ab79726041cdc24dfd46383d6cc16de61507dd
AntoniaBod <143849382+AntoniaBod@users.noreply.github.com>
2024-10-15 16:43:26 -0300

    Update incluirFunc.php

--- TEMPLATES/incluirFunc.php
+++ TEMPLATES/incluirFunc.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="../CSS/style1.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
 <body>
         <div class="container">
