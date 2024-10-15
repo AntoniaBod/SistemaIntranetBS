bb3758e2458c333825628ec7414c0c90b919912a
AntoniaBod <143849382+AntoniaBod@users.noreply.github.com>
2024-10-15 16:50:17 -0300

    Update listarFunc_admin.php

--- TEMPLATES/listarFunc_admin.php
+++ TEMPLATES/listarFunc_admin.php
@@ -37,6 +37,7 @@ if (isset($_GET['id'])) {
     <link rel="stylesheet" href="../CSS/style1.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
 <body>
