4292f08934da43ab8486f70675cfe9bb8e90f026
AntoniaBod <143849382+AntoniaBod@users.noreply.github.com>
2024-10-15 16:46:37 -0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -26,6 +26,8 @@ if(isset($_SESSION['usuario'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Biblioteca de Sabores</title>
     <link rel="stylesheet" href="CSS/styleLogin.css"> <!-- O estilo será gerido no style.css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="container-login">
@@ -55,6 +57,7 @@ if(isset($_SESSION['usuario'])){
             </form>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
 
