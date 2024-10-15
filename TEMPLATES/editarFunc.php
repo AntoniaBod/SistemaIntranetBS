bb3c9e4b4f871d586e07d559acdeb822bedc7058
AntoniaBod <143849382+AntoniaBod@users.noreply.github.com>
2024-10-15 16:43:09 -0300

    Update editarFunc.php

--- TEMPLATES/editarFunc.php
+++ TEMPLATES/editarFunc.php
@@ -28,6 +28,7 @@ if (isset($_GET['id'])) {
     <link rel="stylesheet" href="../CSS/style1.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
 <body>
 <div class="container"> 
